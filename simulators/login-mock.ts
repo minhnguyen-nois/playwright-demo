5b51239a97bfd96bef10ba7dbf62d3261e8e97e8
minhnguyen-nois <minh.nguyen.2@nois.vn>
2025-12-22 14:35:07 +0700

    config login-mock and tsconfig files

--- simulators/login-mock.ts
+++ simulators/login-mock.ts
@@ -1,8 +1,9 @@
-// Dùng để giả lập phản hồi từ server khi login
-export const mockLoginResponse = async (page) => {
-    await page.route('**/api/auth/login', route => route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({ token: 'fake-token-123', user: 'Minh Nguyen' }),
-    }));
-  };
+import { Page, Route } from '@playwright/test'; 
+
+export const mockLoginResponse = async (page: Page) => { 
+  await page.route('**/api/auth/login', (route: Route) => route.fulfill({ 
+    status: 200,
+    contentType: 'application/json',
+    body: JSON.stringify({ token: 'fake-token-123', user: 'Minh Nguyen' }),
+  }));
+};
